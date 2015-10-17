c33986b67b1e56088fa5c18c37653ca27c6800b2
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-10-17 12:38:19 +0200

    Code cleanup

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
-var io = require('socket.io')(http);
+var io = require('socket.io')(http)
 
-var SessionManager = require('./lib/SessionManager').SessionManager;
-var GameServer = require('./lib/GameServer').GameServer;
-
+var SessionManager = require('./lib/SessionManager').SessionManager
+var GameServer = require('./lib/GameServer').GameServer
 
 app.use(express.static(__dirname + '/static'))
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000
 
 http.listen(port, function () {
   console.log('listening on *:3000')
 })
 
 // create the session manager
-var sessions = new SessionManager(io, function(socket1, socket2){
-  new GameServer(socket1, socket2);
-});
+var sessions = new SessionManager(io, function (socket1, socket2) {
+  var gameServer = new GameServer(socket1, socket2)
+})
