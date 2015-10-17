3c1059296cd67e7b5c930e8194b7514074de2ed3
Tom Wiesing <tkw01536@gmail.com>
2015-10-17 12:58:12 +0200

    Update session manager

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 var express = require('express')
-var app = express()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
+var http = require('http')
+var socketIO = require('socket.io')
 
 var SessionManager = require('./lib/SessionManager').SessionManager
 var GameServer = require('./lib/GameServer').GameServer
 
+// create express server and serve the static directory
+var app = express()
 app.use(express.static(__dirname + '/static'))
 
-var port = process.env.PORT || 3000
-
-http.listen(port, function () {
+// create an http server and listen on the right port
+var server = http.Server(app)
+server.listen(process.env.PORT || 3000, function () {
   console.log('listening on *:3000')
 })
 
-// create the session manager
-var sessions = new SessionManager(io, function (socket1, socket2) {
-  var gameServer = new GameServer(socket1, socket2)
+// create a socket io server and run the session manager
+var io = socketIO(server)
+
+// create a session manager
+var sessionManager = new SessionManager()
+sessionManager.on('new_session', function (socket1, socket2) {
+  return new GameServer(socket1, socket2)
 })
+sessionManager.listen(io)
